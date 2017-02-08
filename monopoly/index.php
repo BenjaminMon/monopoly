efe9aa8166c090d02a558feb3ffb2cfc7bf63724
AlexPlt <alex1000.poireault@orange.fr>
2017-02-08 12:48:18 +0100

    /!\ WARNING /!\ CHangeMENt MajeUR ! /!\ WARNING /!\
    LOL

--- monopoly/index.php
+++ monopoly/index.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <title>Jeu Monopoly</title>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+  <link rel="stylesheet" type="text/css" href="css/cs-select.css" />
+	<link rel="stylesheet" type="text/css" href="css/cs-skin-circular.css" />
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
@@ -12,11 +14,11 @@
     <div id="header">
       <img src="Image/bannier.png" id="banniere" alt="bannière" />
     </div>
-    <div id="body">
-      <form action="tableau.php" method="post">
-        <label for="nbJ">Nombre de joueurs : <input type="text" id="nbJ" name="nbJ" />
-        <input type="button" value="Jouer" />
-      </form>
+    <div id="bodyAccueil">
+      <p>
+        <input type="text" placeholder="Pseudo" />
+        <a href="choix.php" class="buttonJoueurAccueil" style="vertical-align:middle"><span>Jouer </span></a>
+      </p>
     </div>
   </div>
 </body>
