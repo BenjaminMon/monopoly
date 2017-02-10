fcc37aad51805d9d38f42332befa472c89f6942c
AlexPlt <alex1000.poireault@orange.fr>
2017-02-10 13:58:34 +0100

    debug 0.0.0.1

--- monopoly/php/pseudo.php
+++ monopoly/php/pseudo.php
@@ -14,10 +14,4 @@ mysqli_set_charset($link, "utf8");
 $listePC = array();
 $req = mysqli_query($link, 'INSERT INTO user (user_pseudo) VALUES ("'.$_GET['pseudo'].'")');
 
-while ($row = mysqli_fetch_array($req, MYSQL_ASSOC)) {
-   $listePC[] = ['joueur_id' => $row['joueur_id'], 'joueur_argent' => $row['joueur_argent'], 'joueur_case_courante' => $row['joueur_case_courante']];
-}
-
-echo json_encode($listePC);
-
 ?>
