8a0f998e8372e65c24f2cde92c75c3bf94204dc0
Julien Marteau <Julien Marteau>
2017-03-21 18:07:37 +0100

    dernières images

--- tableau.php
+++ tableau.php
@@ -36,7 +36,7 @@
         <!-- CASE 3 -->
         <td id="2" rowspan="2" class="relative" onclick="afficherPropriete('')">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          3
+          <p class="rien">CAISSE DE COMMUNAUTE</p><img src="Image/coffre de communauté.png" class="caseTrain" alt="train" />
         </td>
 
         <!-- CASE 4 -->
@@ -48,7 +48,7 @@
         <!-- CASE 5 -->
         <td id="4" rowspan="2" class="relative" onclick="afficherPropriete('')">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          5
+          <p class="rien">IMPOTS SUR LE REVENU</p><p></p><p class="rien">200 €</p>
         </td>
 
         <!-- CASE 6 -->
@@ -66,7 +66,7 @@
         <!-- CASE 8 -->
         <td id="7" rowspan="2" class="relative" onclick="afficherPropriete('')">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          8
+          <p class="rien">CHANCE</p><img src="Image/Chance 2.gif" class="caseTrain" alt="train"/>
         </td>
 
         <!-- CASE 9 -->
@@ -126,14 +126,14 @@
 
       <tr>
         <!-- CASE 3 DE LA LIGNE DE GAUCHE -->
-        <td id="38" colspan="2" class="relative">
+        <td id="38" colspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          3
+          <p class="rien">TAXE DE LUXE</p><img src="Image/Taxe.gif" class="caseTrain" alt="train" />
         </td>
         <!-- CASE 3 DE LA LIGNE DE DROITE -->
-        <td id="12" colspan="2" class="relative">
+        <td id="12" colspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          <p class="Ldroite">9</p>
+          <p class="rien">COMPAGNIE DE DISTRUBUTION D'ELECTRICITE</p><img src="Image/electricite.png" class="caseTrain" alt="train" />
         </td>
       </tr>
 
@@ -154,9 +154,9 @@
 
       <tr>
         <!-- CASE 5 DE LA LIGNE DE GAUCHE -->
-        <td id="36" colspan="2" class="relative">
+        <td id="36" colspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          5
+          <p class="rien">CHANCE</p><img src="Image/Chance 3.gif" class="caseTrain" alt="train" />
         </td>
         <td class="maison3"></td>
         <!-- CASE 5 DE LA LIGNE DE DROITE -->
@@ -196,14 +196,14 @@
 
       <tr>
         <!-- CASE 8 DE LA LIGNE DE GAUCHE -->
-        <td id="33" colspan="2" class="relative">
+        <td id="33" colspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          8
+          <p class="rien">CAISSE DE COMMUNAUTE</p><img src="Image/coffre de communauté.png" class="caseTrain" alt="train" />
         </td>
         <!-- CASE 8 DE LA LIGNE DE DROITE -->
-        <td id="17" colspan="2" class="relative">
+        <td id="17" colspan="2" class="text_centrer relative">
           <!--<img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          <p class="Ldroite">4</p>
+          <p class="rien">CAISSE DE COMMUNAUTE</p><img src="Image/coffre de communauté.png" class="caseTrain" alt="train" />
         </td>
       </tr>
 
@@ -241,14 +241,14 @@
         <!-- CASE 1 -->
         <td id="30" colspan="2" rowspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          Aller en prison
+          <p class="rien">Aller en prison</p>
         </td>
         <!-- CASE 2 -->
         <td class="maison6"></td>
         <!-- CASE 3 -->
-        <td id="28"rowspan="2" class="relative">
+        <td id="28"rowspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          9
+          <p class="rien">COMPAGNIE DE DISTRUBUTION DES EAUX</p><img src="Image/eau.gif" class="caseTrain" alt="train" />
         </td>
         <!-- CASE 4 -->
         <td class="maison6"></td>
@@ -264,16 +264,16 @@
         <!-- CASE 8 -->
         <td class="maison5"></td>
         <!-- CASE 9 -->
-        <td id="22" rowspan="2" class="relative">
+        <td id="22" rowspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          3
+          <p class="rien">CHANCE</p><img src="Image/Chance 1.gif" class="caseTrain" alt="train" />
         </td>
         <!-- CASE 10 -->
         <td class="maison5"></td>
         <!-- CASE 11 -->
         <td id="20" colspan="2" rowspan="2" class="text_centrer relative">
           <!-- JOUEUR ICI : <img src="Image/joueur1.png" class="joueur1 absolute" alt="J1" />-->
-          Parking gratuit
+          <p class="rien">Parking gratuit</p><img src="Image/parc_gratuit.png" class="caseTrain" alt="train" />
         </td>
       </tr>
       <!-- TOUJOURS TROISIEME LIGNE POUR LES COULEURS DES EMPLACEMENTS -->
@@ -324,7 +324,7 @@
   <script src="js/gestionTour.js"></script>
   <script>
 
-   
+
 
     function showRand(){
       var random1 = Math.floor(Math.random() * 6 + 1);
@@ -352,7 +352,7 @@
 
     });
 
-    
+
 
   </script>
   <script>
